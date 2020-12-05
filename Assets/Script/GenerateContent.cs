345796e90a6584176fbfbd67401f51ee325913c8
Cong <hoangduccong8.5@gmail.com>
2020-12-05 15:07:40 +0700

    [DEV] - raw popup dialog

---
+++ Assets/Script/GenerateContent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateContent : MonoBehaviour
+{
+    public GameObject gameObject;
+    public string layerName;
+    static public Dictionary<string, string[]> listParamsEachString = new Dictionary<string, string[]>()
+    {
+        {"Conv2D",new string[]{"Parasm1", "Params2"} }
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerName = "Conv2D";
+        if (listParamsEachString.ContainsKey(layerName))
+        {
+            string[] list = listParamsEachString[layerName];
+            foreach (string val in list)
+            {
+                GameObject it = Instantiate(gameObject, this.transform);
+
+                it.GetComponentInChildren<Text>().text = val;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
