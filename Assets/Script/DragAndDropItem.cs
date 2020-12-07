26d19e0df0ad96da5fa37e1770feba559c8cdfaf
Trieu Truong <khactrieu.personal@gmail.com>
2020-12-07 22:26:52 +0700

    update

--- Assets/Script/DragAndDropItem.cs
+++ Assets/Script/DragAndDropItem.cs
@@ -93,7 +93,7 @@ public class DragAndDropItem : MonoBehaviour, IPointerDownHandler, IBeginDragHan
 
         if (eventData.button == PointerEventData.InputButton.Left)
         {
-            
+
         }
         
 
